bf910e33fbe7fc296522eef48d41386dbd7ef0ee
anasmasti <anasmasti@hotmail.com>
2021-06-08 09:34:19 +0100

    Add list component of proprietaire

--- src/app/pages/proprietaire-page/proprietaire.module.ts
+++ src/app/pages/proprietaire-page/proprietaire.module.ts
@@ -4,11 +4,24 @@ import { CommonModule } from '@angular/common';
 import { ProprietaireComponent } from './proprietaire/proprietaire.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DetailsProprietaireComponent } from './details-proprietaire/details-proprietaire.component';
+import { ListProprietaireComponent } from './list-proprietaire/list-proprietaire.component';
 
-const route: Routes = [{ path: '', component: ProprietaireComponent }];
+
+const route: Routes = [
+  { path: '', component: ProprietaireComponent },
+  { path: 'detail-proprietaire', component: ProprietaireComponent },
+  { path: 'list-proprietaire', component: ProprietaireComponent }
+];
 
 @NgModule({
-  declarations: [ProprietaireComponent, DetailsProprietaireComponent],
-  imports: [RouterModule.forChild(route), CommonModule, ReactiveFormsModule],
+  declarations: [
+    ProprietaireComponent,
+    DetailsProprietaireComponent,
+    ListProprietaireComponent
+  ],
+  imports: [
+    RouterModule.forChild(route),
+    CommonModule, ReactiveFormsModule
+  ],
 })
-export class ProprietaireModule {}
+export class ProprietaireModule { }
