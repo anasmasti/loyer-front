a68c8f6a28883fce845d8dbae0899be27ceba86c
youssefsakini <youssef0sakini@gmail.com>
2021-06-09 13:50:23 +0100

    Fix validation form proprietaire & add button go to list proprietaire in details proprietaire

--- src/app/pages/proprietaire-page/proprietaire/proprietaire.component.ts
+++ src/app/pages/proprietaire-page/proprietaire/proprietaire.component.ts
@@ -22,28 +22,52 @@ export class ProprietaireComponent implements OnInit {
 
   proprietaireForm: any = new FormGroup({
     // Champs du propriètaire
-    cin: new FormControl('', Validators.required),
-    passport: new FormControl('', Validators.required),
-    carte_sejour: new FormControl('', Validators.required),
-    nom_prenom: new FormControl('', Validators.required),
-    raison_social: new FormControl('', Validators.required),
-    n_registre_commerce: new FormControl('', Validators.required),
-    telephone: new FormControl('', Validators.required),
-    fax: new FormControl('', Validators.required),
-    adresse: new FormControl('', Validators.required),
-    n_compte_bancaire: new FormControl('', Validators.required),
-    banque: new FormControl('', Validators.required),
-    nom_agence_bancaire: new FormControl('', Validators.required),
-    has_mandataire: new FormControl('', Validators.required),
+    cin: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    passport: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+    ]),
+    carte_sejour: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+    ]),
+    nom_prenom: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[a-zA-Z ]*'),
+    ]),
+    raison_social: new FormControl('', [Validators.required]),
+    n_registre_commerce: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[a-zA-Z0-9]*'),
+    ]),
+    telephone: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[0-9]*'),
+    ]),
+    fax: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[0-9]*'),
+    ]),
+    adresse: new FormControl('', [Validators.required]),
+    n_compte_bancaire: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[0-9]*'),
+    ]),
+    banque: new FormControl('', [Validators.required]),
+    nom_agence_bancaire: new FormControl('', [Validators.required]),
+    has_mandataire: new FormControl('', [Validators.required]),
     // Champs du mandataire
     mandataire: new FormGroup({
-      cin_mandataire: new FormControl(''),
-      nom_prenom_mandataire: new FormControl(''),
+      cin_mandataire: new FormControl('',Validators.minLength(4)),
+      nom_prenom_mandataire: new FormControl('',Validators.pattern('[a-zA-Z]*')),
       raison_social_mandataire: new FormControl(''),
-      telephone_mandataire: new FormControl(''),
-      fax_mandataire: new FormControl(''),
+      telephone_mandataire: new FormControl('', Validators.pattern('[0-9]*')),
+      fax_mandataire: new FormControl('', Validators.pattern('[0-9]*')),
       adresse_mandataire: new FormControl(''),
-      n_compte_bancaire_mandataire: new FormControl(''),
+      n_compte_bancaire_mandataire: new FormControl(
+        '',
+        Validators.pattern('[0-9]*')
+      ),
     }),
   });
 
@@ -58,5 +82,72 @@ export class ProprietaireComponent implements OnInit {
         console.log(error.error.message);
       }
     );
+  }
+
+  // Check if all inputs has invalid errors
+  checkInputsValidation(targetInput: any) {
+    return targetInput?.invalid && (targetInput.dirty || targetInput.touched);
+  }
+
+  get cin() {
+    return this.proprietaireForm.get('cin');
+  }
+  get passport() {
+    return this.proprietaireForm.get('passport');
+  }
+  get carte_sejour() {
+    return this.proprietaireForm.get('carte_sejour');
+  }
+  get nom_prenom() {
+    return this.proprietaireForm.get('nom_prenom');
+  }
+  get raison_social() {
+    return this.proprietaireForm.get('raison_social');
+  }
+  get n_registre_commerce() {
+    return this.proprietaireForm.get('n_registre_commerce');
+  }
+  get telephone() {
+    return this.proprietaireForm.get('telephone');
+  }
+  get fax() {
+    return this.proprietaireForm.get('fax');
+  }
+  get adresse() {
+    return this.proprietaireForm.get('adresse');
+  }
+  get n_compte_bancaire() {
+    return this.proprietaireForm.get('n_compte_bancaire');
+  }
+  get banque() {
+    return this.proprietaireForm.get('banque');
+  }
+  get nom_agence_bancaire() {
+    return this.proprietaireForm.get('nom_agence_bancaire');
+  }
+  get has_mandataire() {
+    return this.proprietaireForm.get('has_mandataire');
+  }
+  // Mandataire
+  get cin_mandataire() {
+    return this.proprietaireForm.get('cin_mandataire');
+  }
+  get nom_prenom_mandataire() {
+    return this.proprietaireForm.get('nom_prenom_mandataire');
+  }
+  get raison_social_mandataire() {
+    return this.proprietaireForm.get('raison_social_mandataire');
+  }
+  get telephone_mandataire() {
+    return this.proprietaireForm.get('telephone_mandataire');
+  }
+  get fax_mandataire() {
+    return this.proprietaireForm.get('fax_mandataire');
+  }
+  get adresse_mandataire() {
+    return this.proprietaireForm.get('adresse_mandataire');
+  }
+  get n_compte_bancaire_mandataire() {
+    return this.proprietaireForm.get('n_compte_bancaire_mandataire');
   }
 }
