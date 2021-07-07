ee50300700ae78e4156244e4885f3dae4270d489
anasmasti <anasmasti@hotmail.com>
2021-07-07 12:28:22 +0100

    Add notification and dashboard pages
    - Add notification component service and module and setup route
    - Add dashboard component with service and module
    - Add notification and dashboard to main route and to the main navigation bar

---
+++ src/app/shared/notification/main-notifications/main-notifications.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MainNotificationsComponent } from './main-notifications.component';
+
+describe('MainNotificationsComponent', () => {
+  let component: MainNotificationsComponent;
+  let fixture: ComponentFixture<MainNotificationsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MainNotificationsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MainNotificationsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
