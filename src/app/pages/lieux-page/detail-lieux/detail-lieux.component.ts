71486a993bed62abfba0a5b5f04b1275be8f0687
anasmasti <anasmasti@hotmail.com>
2021-07-08 16:10:00 +0100

    consomation d'api -remplissage de service de lieu -creation des fonctions d'affichage des données

--- src/app/pages/lieux-page/detail-lieux/detail-lieux.component.ts
+++ src/app/pages/lieux-page/detail-lieux/detail-lieux.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ConfirmationModalService } from './../../../services/confirmation-modal.service';
+import { MainModalService } from './../../../services/main-modal.service';
+import { LieuxService } from 'src/app/services/lieux.service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-detail-lieux',
   templateUrl: './detail-lieux.component.html',
   styleUrls: ['./detail-lieux.component.scss']
 })
 export class DetailLieuxComponent implements OnInit {
-
-  constructor() { }
+  Lieu:any=[];
+  constructor(
+    private lieuxService: LieuxService,
+    private mainModalService: MainModalService,
+    private confirmationModalService: ConfirmationModalService,
+    private actRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.getLieuById();
   }
+// Get the proprietaire data by id
+getLieuById() {
+  const id = this.actRoute.snapshot.paramMap.get('id') || '';
+  this.lieuxService.getLieuById(id).subscribe((data:any) => {
+    this.Lieu = data;
+  });
+  console.log(id);
+  console.log(this.Lieu);
+
+}
 
 }
--- src/app/services/lieux.service.ts
+++ src/app/services/lieux.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Lieu } from '../models/lieu';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LieuxService {
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  
+  // Get list of all proprietaires from database
+  getLieux(){
+    return this.http.get('http://localhost:5000/api/v1/Lieu/tout');
+  }
+  getLieuById(id:any){
+    return this.http.get('http://localhost:5000/api/v1/Lieu/'+id);
+  }
+
+
+
+
 }
