b2a9072f2b6119f6c90bf2e9074f4903315305cc
youssefsakini <youssef0sakini@gmail.com>
2021-07-09 12:24:13 +0100

    Setup list-contrat

--- src/app/pages/contrat-page/list-contrat/list-contrat.component.ts
+++ src/app/pages/contrat-page/list-contrat/list-contrat.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfirmationModalService } from 'src/app/services/confirmation-modal.service';
+import { ContratService } from 'src/app/services/contrat.service';
+import { MainModalService } from 'src/app/services/main-modal.service';
 
 @Component({
   selector: 'app-list-contrat',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListContratComponent implements OnInit {
 
-  constructor() { }
+  contrats: any = [];
 
+  constructor(
+    private contratService: ContratService,
+    private mainModalService: MainModalService,
+    private confirmationModalService: ConfirmationModalService
+  ) { }
   ngOnInit(): void {
+   setTimeout(() => {
+      this.getAllLieux();
+   }, 400);
+   
   }
+
+  getAllLieux(){
+    this.contratService.getContrat().subscribe((data:any) => {
+      this.contrats = data;
+    });
+    // console.log('liste des lieux : '+this.lieux);
+  }
+  openConfirmationModal() {
+    this.confirmationModalService.open(); // Open delete confirmation modal
+  }
+
+  // Close confirmation modal
+  closeConfirmationModal() {
+    this.confirmationModalService.close(); // Close delete confirmation modal
+  }
+
 
 }
--- src/app/services/contrat.service.ts
+++ src/app/services/contrat.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,20 @@ import { Injectable } from '@angular/core';
 })
 export class ContratService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  
+  // Get list of all proprietaires from database
+  getContrat(){
+    return this.http.get('http://localhost:5000/api/v1/contrat');
+  }
+
+  // get specific "lieu" by his id 
+  getContratById(id:any){
+    return this.http.get('http://localhost:5000/api/v1/contrat/'+id);
+  }
+
+
+  
+
 }
