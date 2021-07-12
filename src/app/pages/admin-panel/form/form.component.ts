6dba720d1a9f10eb9a0f9b062abd8f6eaa72d4de
anasmasti <anasmasti@hotmail.com>
2021-07-12 12:57:31 +0100

    Modification de admin component -definition de formGroupe et formControls

--- src/app/pages/admin-panel/form/form.component.ts
+++ src/app/pages/admin-panel/form/form.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
+
+  adminForm: any = new FormGroup({
+    // Champs du propriètaire
+    Matricule: new FormControl('', []),
+    NomComplet: new FormControl('', [
+    ]),
+    Role: new FormControl('', [
+    ]),
+   
+  });
 
   constructor() { }
 
