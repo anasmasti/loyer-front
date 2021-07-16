26077f01559d8decd4d5e9090f28dc26c6f78657
youssefsakini <youssef0sakini@gmail.com>
2021-07-16 15:39:21 +0100

    Init commit

--- src/app/services/lieux-service/lieux.service.ts
+++ src/app/services/lieux-service/lieux.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-import { Lieu } from '../../models/Lieu';
+import { HttpClient, HttpHeaders } from '@angular/common/http'; 
+import { Lieu } from '../../models/lieu';
 import { Observable } from 'rxjs';
-import { stringify } from '@angular/compiler/src/util';
+
 
 @Injectable({
   providedIn: 'root',
@@ -20,17 +19,9 @@ export class LieuxService {
   getLieuById(id: String): Observable<Lieu> {
     return this.http.get<Lieu>('http://localhost:5000/api/v1/Lieu/' + id);
   }
-<<<<<<< HEAD
 
   postDR(data:any){
      return this.http.post('http://192.168.11.124:5000/api/v1/lieu/ajouter',data);
   }
 
- 
-
-
-
-
-=======
->>>>>>> 9484caa2d73faa8f0cf618cfc1d65e3a61b0139e
 }
