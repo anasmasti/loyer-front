d455f5d97a63ee6667827087eafdf374ec5c230c
anasmasti <anasmasti@hotmail.com>
2021-07-19 16:39:45 +0100

    get test Add Lieu changes

--- src/app/pages/lieux-page/forms/dr-form/dr-form.component.ts
+++ src/app/pages/lieux-page/forms/dr-form/dr-form.component.ts
@@ -17,43 +17,6 @@ export class DrFormComponent implements OnInit {
   // drForm !: FormGroup;
   constructor(private drService: LieuxService) { }
 
-<<<<<<< HEAD
-  drForm: FormGroup = new FormGroup({
-    code_lieu: new FormControl('',),
-    intitule_lieu: new FormControl('',),
-    // intitule_DR: new FormControl('',),
-    adresse: new FormControl('',),
-    ville: new FormControl('',),
-    code_localite: new FormControl('',),
-    desc_lieu_entrer: new FormControl('',),
-    // imgs_lieu_entrer: new FormControl('',),
-    has_amenagements: new FormControl('',),
-    superficie: new FormControl('',),
-    telephone: new FormControl('',),
-    fax: new FormControl('',),
-    etage: new FormControl('',),
-    type_lieu: new FormControl('',),
-    code_rattache_DR: new FormControl('',),
-    code_rattahce_SUP: new FormControl('',),
-    intitule_rattache_SUP_PV: new FormControl('',),
-    centre_cout_siege: new FormControl('',),
-    categorie_pointVente: new FormControl('',),
-    deleted: new FormControl('',),
-
-    nature_amenagement: new FormControl(''),
-    montant_amenagement: new FormControl(''),
-    valeur_nature_chargeProprietaire: new FormControl(''),
-    valeur_nature_chargeFondation: new FormControl(''),
-    numero_facture: new FormControl(''),
-    numero_bon_commande: new FormControl(''),
-    date_passation_commande: new FormControl(''),
-    evaluation_fournisseur: new FormControl(''),
-    date_fin_travaux: new FormControl(''),
-    date_livraison_local: new FormControl(''),
-  })
-=======
-
->>>>>>> 510b91c12064104e9beea2c7cfa5d7a2fd84b521
 
   ngOnInit(): void {
     // console.log(this.drForm)
@@ -148,13 +111,8 @@ export class DrFormComponent implements OnInit {
   addDR() {
     // this.$testDrForm = this.drService.postDR(this.drForm)
     // this.$testDrForm.subscribe()
-<<<<<<< HEAD
-    let formdata = new FormData();
-    formdata.append('imgs_lieu_entrer', this.selectedFile, this.selectedFile.name)
-=======
     // let formdata = new FormData();
     // formdata.append('imgs_lieu_entrer', this.selectedFile, this.selectedFile.name)
->>>>>>> 510b91c12064104e9beea2c7cfa5d7a2fd84b521
     // let myform = JSON.stringify(this.drForm.value)
     // let myform=JSON.parse(this.drForm.value)
 
