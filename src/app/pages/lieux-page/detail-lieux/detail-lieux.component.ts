212da0eee4af643f0e29e4e080edbdffdb39382b
anasmasti <anasmasti@hotmail.com>
2021-08-11 11:06:07 +0100

    Change detail Proprietaire UI

--- src/app/pages/lieux-page/detail-lieux/detail-lieux.component.ts
+++ src/app/pages/lieux-page/detail-lieux/detail-lieux.component.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../../../environments/environment';
 import { Lieu } from 'src/app/models/Lieu';
 
 import { Component, OnInit } from '@angular/core';
@@ -11,102 +12,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailLieuxComponent implements OnInit {
 
-  lieu: Lieu = {
-    _id: 'Chargement...',
-    code_lieu: 'Chargement...',
-    intitule_lieu: 'Chargement...',
-    intitule_DR: 'Chargement...',
-    adresse: 'Chargement...',
-    ville: 'Chargement...',
-    code_localite: 'Chargement...',
-    desc_lieu_entrer: 'Chargement...',
-    imgs_lieu_entrer: [
-      {
-        _id: 'Chargement...',
-        image: 'Chargement...'
-      }
-    ],
-    has_amenagements: false,
-    superficie: 'Chargement...',
-    telephone: 'Chargement...',
-    fax: 'Chargement...',
-    etat_logement_fonction: 'Chargement...',
-    etage: 'Chargement...',
-    type_lieu: 'Chargement...',
-    code_rattache_DR: 'Chargement...',
-    code_rattache_SUP: 'Chargement...',
-    intitule_rattache_SUP_PV: 'Chargement...',
-    centre_cout_siege: 'Chargement...',
-    categorie_pointVente: 'Chargement...',
-    deleted: false,
-
-    directeur_regional: [
-      {
-        matricule: 'Chargement...',
-        nom: 'Chargement...',
-        prenom: 'Chargement...',
-        deleted: false
-      }
-    ],
-
-    amenagement: [
-      {
-        _id: 'Chargement...',
-        nature_amenagement: 'Chargement...',
-        montant_amenagement: 'Chargement...',
-        valeur_nature_chargeProprietaire: 'Chargement...',
-        valeur_nature_chargeFondation: 'Chargement...',
-        numero_facture: 'Chargement...',
-        numero_bon_commande: 'Chargement...',
-        date_passation_commande: 'Chargement...',
-        evaluation_fournisseur: 'Chargement...',
-        date_fin_travaux: 'Chargement...',
-        date_livraison_local: 'Chargement...',
-        deleted: false,
-
-        fournisseur: [{
-          nom: 'Chargement...',
-          prenom: 'Chargement...',
-          amenagement_effectue: 'Chargement...',
-          deleted: false,
-        }]
-      }
-    ]
-
-  };
-
-  selectedAmenagement: any = {
-    _id: 'Chargement...',
-    nature_amenagement: 'Chargement...',
-    montant_amenagement: 'Chargement...',
-    valeur_nature_chargeProprietaire: 'Chargement...',
-    valeur_nature_chargeFondation: 'Chargement...',
-    numero_facture: 'Chargement...',
-    numero_bon_commande: 'Chargement...',
-    date_passation_commande: 'Chargement...',
-    evaluation_fournisseur: 'Chargement...',
-    date_fin_travaux: 'Chargement...',
-    date_livraison_local: 'Chargement...',
-    deleted: false,
-
-    fournisseur: [
-      {
-        nom: 'Chargement...',
-        prenom: 'Chargement...',
-        amenagement_effectue: 'Chargement...',
-        deleted: false,
-      }
-    ]
-  };
-
+  lieu!: Lieu;
+  selectedAmenagement!: any;
   displayAmenagementSection: boolean = false;
-
-  selectedImageEntrer: any = [
-    {
-      _id: 'Chargement...',
-      image: 'Chargement...'
-    }
-  ]
+  selectedImageEntrer!: any;
+  url: string = 'http://192.168.11.118:5000/';
 
   constructor(
     private lieuxService: LieuxService,
