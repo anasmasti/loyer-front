f18dce766dd1fdfcad926c927146530f6244f11b
anasmasti <anasmasti@hotmail.com>
2021-09-07 11:12:00 +0100

    Setup guard route

--- src/app/middleware/roles/check-roles.guard.ts
+++ src/app/middleware/roles/check-roles.guard.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckRolesGuard implements CanActivate {
+
+  x!: boolean
+  
+  constructor(public router: Router) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      
+      if (this.x == true) {
+        this.router.navigate(['login']);
+        return false;
+      }
+
     return true;
   }
   
