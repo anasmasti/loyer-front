26cf724967a33f416a6ec42e56ce5d49c0018d8d
anasmasti <anasmasti@hotmail.com>
2021-12-06 09:47:27 +0100

    Add reporting service with functions

---
+++ src/app/services/reporting/reporting.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReportingService {
+  param_url: string = 'reporting';
+
+  constructor(private http: HttpClient) { }
+
+  // Generate reportings
+  generateReportings(matricule: any, data: any): Observable<any> {
+    return this.http.post<any>(
+      `${environment.API_URL_TEST + environment.API_VERSION + this.param_url
+      }/generate/${matricule}`, data
+    );
+  }
+
+  downloadLieuxReporting() {
+    console.log('done');
+    
+  }
+
+  downloadContratReporting() {
+    console.log('done');
+    
+  }
+
+}
