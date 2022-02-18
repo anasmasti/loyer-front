db1b1719e28c179d2c709d61f1770fdc5b2f1e3b
youssefsakini <youssef0sakini@gmail.com>
2022-02-18 13:01:32 +0100

    Add tooltips to button in header-navbar

--- src/app/pages/proprietaire-page/list-proprietaire/list-proprietaire.component.ts
+++ src/app/pages/proprietaire-page/list-proprietaire/list-proprietaire.component.ts
@@ -5,7 +5,6 @@ import { Proprietaire } from '../../../models/Proprietaire';
 import { Component, OnInit } from '@angular/core';
 import { ProprietaireService } from 'src/app/services/proprietaire-service/proprietaire.service';
 import { ContratService } from '@services/contrat-service/contrat.service';
-import { LoginModule } from '../../login-page/login.module';
 
 @Component({
   selector: 'app-list-proprietaire',
@@ -22,16 +21,16 @@ export class ListProprietaireComponent implements OnInit {
 
   //Delete succes message
   deleteDone: boolean = false;
-  deleteSucces: string = 'Proprietaire supprimé avec succés'
+  deleteSucces: string = 'Proprietaire supprimé avec succés';
 
   // Pagination options
   listProprietairePage: number = 1;
   count: number = 0;
   tableSize: number = 10;
 
-  userMatricule: any = localStorage.getItem('matricule')
+  userMatricule: any = localStorage.getItem('matricule');
 
-  id: string = "DeleteConfirmation"
+  id: string = 'DeleteConfirmation';
 
   contrats!: any;
 
@@ -41,12 +40,12 @@ export class ListProprietaireComponent implements OnInit {
     private mainModalService: MainModalService,
     private confirmationModalService: ConfirmationModalService,
     private helperService: HelperService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     setTimeout(() => {
       this.getAllProprietaires();
-    }, 1000);// Trow the fitching data    
+    }, 1000); // Trow the fitching data
   }
 
   // ngOnChanges() {
@@ -57,7 +56,7 @@ export class ListProprietaireComponent implements OnInit {
   // getAllProprietaires() {
   //   this.proprietaireService.getProprietaire(this.userMatricule).subscribe((data) => {
   //     this.contrats = data;
-  //     this.proprietaires = this.contrats[0].foncier.proprietaire 
+  //     this.proprietaires = this.contrats[0].foncier.proprietaire
   //   }, error => {
   //     this.accessError = error.error.message
   //   });
@@ -66,27 +65,39 @@ export class ListProprietaireComponent implements OnInit {
   // Filter by intitule
   search() {
     console.log(this.proprietaires);
-    
-    if (this.findProprietaire != "") {
-      this.proprietaires = this.proprietaires.filter(res => {
-        return res.cin?.toLowerCase().match(this.findProprietaire.toLowerCase()) || res.passport?.toLowerCase().match(this.findProprietaire.toLowerCase())
-          || res.carte_sejour?.toLowerCase().match(this.findProprietaire.toLowerCase()) || res.nom_prenom?.toLowerCase().match(this.findProprietaire.toLowerCase());
+    if (this.findProprietaire != '') {
+      this.proprietaires = this.proprietaires.filter((res) => {
+        console.log(res.passport);
+
+        return res.cin
+          ?.toLowerCase()
+          .match(this.findProprietaire.toLowerCase());
+        // res.passport?.toLowerCase().match(this.findProprietaire.toLowerCase()) ||
+        // res.carte_sejour?.toLowerCase().match(this.findProprietaire.toLowerCase()) ||
+        // res.nom_prenom?.toLowerCase().match(this.findProprietaire.toLowerCase())
       });
-    } else if (this.findProprietaire == "") {
+    } else if (this.findProprietaire == '') {
       this.getAllProprietaires();
     }
   }
 
   // Get data from proprietaire service
   getAllProprietaires() {
-    this.proprietaireService.getProprietaire(this.userMatricule).subscribe((data) => {
-      this.contrats = data;
-      console.log(data);
-      
-      this.proprietaires = this.contrats[0].foncier.proprietaire 
-    }, error => {
-      this.accessError = error.error.message
-    });
+    this.proprietaireService.getProprietaire(this.userMatricule).subscribe(
+      (data) => {
+        this.contrats = data;
+
+        // console.log('data =>', data);
+        // data.forEach((proprietaire: any) => {
+        //   this.proprietaires.push(proprietaire.foncier.proprietaire);
+        //   let test: any[] = [];
+        //   console.log('proprietaire =>', test.concat(...this.proprietaires));
+        // });
+      },
+      (error) => {
+        this.accessError = error.error.message;
+      }
+    );
   }
 
   // Open the update proprietaire form and push index and data of proprietaire
@@ -96,7 +107,7 @@ export class ListProprietaireComponent implements OnInit {
   }
 
   checkAndPutText(value: boolean) {
-    return this.helperService.booleanToText(value)
+    return this.helperService.booleanToText(value);
   }
 
   // Open confirmation modal
@@ -124,23 +135,25 @@ export class ListProprietaireComponent implements OnInit {
       deleted: true,
     };
     // Call detele proprietaire function from proprietaire service
-    this.proprietaireService.deleteProprietaire(id, data, this.userMatricule).subscribe(
-      (_) => {
-        this.getAllProprietaires(); // Trow the fitching data
-        this.closeDeleteConfirmationModal()
-        this.deleteDone = true;
-        setTimeout(() => {
-          this.deleteDone = false;
-        }, 3000);
-      },
-      (error) => {
-        this.errors = error.error.message;
-        setTimeout(() => {
-          this.showErrorMessage();
-        }, 3000);
-        this.hideErrorMessage();
-      }
-    );
+    this.proprietaireService
+      .deleteProprietaire(id, data, this.userMatricule)
+      .subscribe(
+        (_) => {
+          this.getAllProprietaires(); // Trow the fitching data
+          this.closeDeleteConfirmationModal();
+          this.deleteDone = true;
+          setTimeout(() => {
+            this.deleteDone = false;
+          }, 3000);
+        },
+        (error) => {
+          this.errors = error.error.message;
+          setTimeout(() => {
+            this.showErrorMessage();
+          }, 3000);
+          this.hideErrorMessage();
+        }
+      );
   }
 
   // Get id of selected proprietaire
