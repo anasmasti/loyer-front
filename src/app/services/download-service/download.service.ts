19bc3d967219857f836acae103eb3e39628bfa6c
YassineAm00 <yassine.19am49@gmail.com>
2022-04-05 14:25:49 +0000

    Fix download excel file issues

--- src/app/services/download-service/download.service.ts
+++ src/app/services/download-service/download.service.ts
@@ -5,13 +5,14 @@ import { saveAs } from 'file-saver';
 import { async } from '@angular/core/testing';
 import { HelperService } from '@services/helpers/helper.service';
 import dateClotureType from 'src/app/pages/cloture/date-cloture.type';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DownloadService {
   dateCloture!: dateClotureType;
-  constructor(private http: HttpClient, private help: HelperService) {}
+  constructor(private http: HttpClient) {}
 
   setFilename(filename: string) {
     const param = new HttpParams().set('filename', filename);
@@ -33,14 +34,8 @@ export class DownloadService {
     );
   }
 
-  // Get the next cloture date from the server
-  getNextCloture() {
-    this.help.getNextClotureDate().subscribe((date: dateClotureType) => {
-      this.dateCloture = date;
-    });
-  }
-  
-  dowloadExcelFiles(filename: string) {
+
+  dowloadExcelFiles(filename: string, mois: number, annee:number) {
     // let date = new Date();
     return this.http
       .get(
@@ -49,7 +44,7 @@ export class DownloadService {
           environment.API_VERSION +
           'download-excel/' +
           filename
-        }/${this.dateCloture.mois}/${this.dateCloture.annee}`,
+        }/${mois}/${annee}`,
         {
           responseType: 'blob',
         }
