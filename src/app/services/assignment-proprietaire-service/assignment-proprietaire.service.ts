774df985b52a77feb30d4b50031a59cbcb0afa08
youssefsakini <youssef0sakini@gmail.com>
2022-04-25 15:15:29 +0000

    Fix add assignement proprietaire

--- src/app/services/assignment-proprietaire-service/assignment-proprietaire.service.ts
+++ src/app/services/assignment-proprietaire-service/assignment-proprietaire.service.ts
@@ -8,7 +8,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class AssignmentProprietaireService {
-  param_url: string = 'assignment';
+  param_url: string = 'affectation-proprietaire';
   
   constructor(private http: HttpClient) { }
 
