8e3e435c282dee73c395fdec756cce705c635fc7
anasmasti <anasmasti@hotmail.com>
2022-05-25 15:21:06 +0100

    Get changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -19,7 +19,7 @@ export const environment = {
   // Reporting
   REPORTING: true,
 };
-/*
+/*                                     
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
