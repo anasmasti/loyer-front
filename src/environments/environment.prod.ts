942b806ba429b4a71219b1ed9fe0785bc1013274
anasmasti <anasmasti@hotmail.com>
2022-07-13 10:18:21 +0100

    Add changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,16 @@
 export const environment = {
   production: true,
   // Api url
-  API_URL: 'https://loyer-api.herokuapp.com/api/',
-  API_URL_TEST: 'http://localhost:8000/api/',
+  API_URL: 'https://loyer-app.herokuapp.com/api/',
+  API_URL_TEST: 'https://loyer-app.herokuapp.com/api/',
   // Api test url without param
-  API_URL_WITHOUT_PARAM: 'http://localhost:8000/',
+  API_URL_WITHOUT_PARAM: 'https://loyer-app.herokuapp.com/',
   // Api access key
   API_ACCESS_KEY: 'take$the#whole!bull',
   // Api version
   API_VERSION: 'v1/',
   // Application version
   APP_VERSION: '1 (Beta 6.1)',
-   // Reporting
-   REPORTING: true,
+  // Reporting
+  REPORTING: true,
 };
