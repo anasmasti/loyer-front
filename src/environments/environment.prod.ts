b7330562a049c3fdb51fa5d164b4c8552296b5b3
anasmasti <anasmasti@hotmail.com>
2022-07-27 10:49:04 +0100

    Get changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
   // Api url
   API_URL: 'https://loyer-api.herokuapp.com/api/',
-  API_URL_TEST: 'http://localhost:8000/api/',
+  API_URL_TEST: 'http://192.168.11.101/api/',
   // Api test url without param
-  API_URL_WITHOUT_PARAM: 'http://localhost:8000/',
+  API_URL_WITHOUT_PARAM: 'http://192.168.11.101/',
   // Api access key
   API_ACCESS_KEY: 'take$the#whole!bull',
   // Api version
